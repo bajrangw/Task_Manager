7fba7d2b66226ac394dc1b260b313506bf2e7024
bajrangw <bajrangwavare@gmail.com>
2025-09-17 21:29:05 +0530

    Refactor package.json and vite.config.js: Remove unused lint script and tailwindcss plugin

--- frontend/task-manager/vite.config.js
+++ frontend/task-manager/vite.config.js
@@ -1,10 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [react()],
+  base: '/',       // important for routing on Vercel
+  build: {
+    outDir: 'dist' // ensure matches vercel.json distDir
+  }
 })
-
-
