2d2190b7887cce9580eedd3d8e00978f3ecd2322
bajrangw <bajrangwavare@gmail.com>
2025-09-18 17:50:38 +0530

    Refactor axiosInstance.js: Update baseURL to include '/api' for correct API endpoint

--- frontend/task-manager/src/utils/axiosInstance.js
+++ frontend/task-manager/src/utils/axiosInstance.js
@@ -4,7 +4,7 @@ import axios from "axios";
 const API_BASE = import.meta.env.VITE_BACKEND_URL || "http://localhost:8000";
 
 const axiosInstance = axios.create({
-    baseURL: API_BASE,
+    baseURL: `${API_BASE}/api`,
     timeout: 10000,
     headers: {
         "Content-Type": "application/json",
