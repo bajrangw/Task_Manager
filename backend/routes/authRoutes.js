167c5782bb72731bb86e1b32846e24f888f773ce
bajrangw <bajrangwavare@gmail.com>
2025-09-18 20:42:14 +0530

    #

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -11,14 +11,26 @@ router.post("/login", loginUser); // Login User
 router.get("/profile", protect, getUserProfile); // Get User Profile
 router.put("/profile", protect, updateUserProfile); // Update Profile
 
+// router.post("/upload-image", upload.single("image"), (req, res) => {
+//     if (!req.file) {
+//         return res.status(400).json({ message: "No file uploaded" });
+//     }
+//     const imageUrl = `${req.protocol}://${req.get("host")}/uploads/${
+//         req.file.filename
+//     }`;
+//     res.status(200).json({ imageUrl });
+// });
+
 router.post("/upload-image", upload.single("image"), (req, res) => {
     if (!req.file) {
         return res.status(400).json({ message: "No file uploaded" });
     }
-    const imageUrl = `${req.protocol}://${req.get("host")}/uploads/${
-        req.file.filename
-    }`;
+
+    const backendUrl = process.env.BACKEND_URL || `${req.protocol}://${req.get("host")}`;
+    const imageUrl = `${backendUrl}/uploads/${req.file.filename}`;
+
     res.status(200).json({ imageUrl });
 });
+
 
 module.exports = router;
