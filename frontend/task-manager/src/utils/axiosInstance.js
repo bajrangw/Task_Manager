094b4b34fa3eca24278633c0201cde5add7a178f
bajrangw <bajrangwavare@gmail.com>
2025-09-18 17:53:45 +0530

    #

--- frontend/task-manager/src/utils/axiosInstance.js
+++ frontend/task-manager/src/utils/axiosInstance.js
@@ -4,7 +4,7 @@ import axios from "axios";
 const API_BASE = import.meta.env.VITE_BACKEND_URL || "http://localhost:8000";
 
 const axiosInstance = axios.create({
-    baseURL: `${API_BASE}/api`,
+    baseURL: API_BASE,
     timeout: 10000,
     headers: {
         "Content-Type": "application/json",
